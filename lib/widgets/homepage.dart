b9caa12eaa66cc5cb42be105873fbf2534a01330
ntl22 <nxtung16@outlook.com>
2022-11-26 09:48:19 +0700

    add homepage template

---
+++ lib/widgets/homepage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UIT Hackathon',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const HomePage(title: 'UIT'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('This button is pushed many times: '),
+            Text('$_counter')
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      )
+    );
+  }
+}
+
